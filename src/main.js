8e937ab80b0a9524eeb95326dfb4019d57add212
Plightovr <igadsden@brooklynsteamcenter.org>
2026-05-28 14:53:51 -0400

    added upgrades

--- src/main.js
+++ src/main.js
@@ -2,9 +2,57 @@ let score= 0
 
 let clicker = document.getElementById('clicker')
 
+
+
 let scoreDisplay = document.getElementById('scoreDisplay')
 
+function updateScore (amount) {
+  score+= amount
+  scoreDisplay.innerText = score.toFixed(0) + 'cargo'
+}
+
 clicker.addEventListener("click", function(){
-  score++
-  scoreDisplay.innerText = score
+  let clickAmt = 1 + (hooks ** 1.05)
+  
+  updateScore(clickAmt)
+  
+})
+
+
+let upgradeOne = document.getElementById("upgradeOne")
+let upgradeOneCount = document.getElementById("upgradeOneCount")
+let hooks = 0
+
+upgradeOne.addEventListener("click", function () {
+  if (score >= 112) {
+    
+    updateScore(-112)
+    hooks++
+    upgradeOneCount.innerText = hooks + "hooks"
+  } else {
+    alert('no')
+  }
+ 
+})
+clicker.addEventListener("click", function(){
+  let clickAmt = 1 + (hooks ** 1.05) + (redhooks ** 1.70)
+  
+  updateScore(clickAmt)
+  
+})
+
+let upgradeTwo = document.getElementById("upgradeTwo")
+let upgradeTwoCount = document.getElementById("upgradeTwoCount")
+let redhooks = 0
+
+upgradeTwo.addEventListener("click", function () {
+  if (score >= 730) {
+    
+    updateScore(-730)
+    redhooks++
+    upgradeTwoCount.innerText = redhooks + "redhooks"
+  } else {
+    alert('no bro')
+  }
+ 
 })
